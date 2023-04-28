d8a8675b7fb369229b12b30abad042e1d0a5c5f0
vganapati <vganapa1@swarthmore.edu>
2023-04-28 14:36:28 -0700

    Update train_perlmutter.sh

--- scripts/train_perlmutter.sh
+++ scripts/train_perlmutter.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 #SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -J pytorch       # job name
+#SBATCH -L SCRATCH       # job requires SCRATCH files
+#SBATCH -A m2859_g       # allocation
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-node=4
-#SBATCH --time 30
+#SBATCH -t 00:36:00
 #SBATCH -J train-pm
 #SBATCH -o logs/%x-%j.out
 
 # Setup software
-module load pytorch/1.11.0
+module load pytorch/1.13.1
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=29500
 
+echo "jobstart $(date)";pwd
+
 # Run the training
 srun -l -u python train.py -d nccl --rank-gpu --ranks-per-node=${SLURM_NTASKS_PER_NODE} $@
+
+echo "jobend $(date)";pwd
